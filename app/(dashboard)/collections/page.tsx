189f7aa75e55ba97996b8026132194d4556fa343
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2024-08-30 17:16:07 +0800

    add get collections frontend

--- app/(dashboard)/collections/page.tsx
+++ app/(dashboard)/collections/page.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Breadcrumbs } from '@/components/breadcrumbs'
+import PageContainer from '@/components/layout/page-container';
+
+const breadcrumbItems = [
+    { title: "Dashboard", link: "/" },
+    { title: "Collections", link: "/collections" }
+];
 
 const Collection = () => {
+  const [loading, setLoading] = useState(true)
+  const [collections, setCollections] = useState([])
+
+  const getCollections = async () => {
+    try {
+        const res = await fetch("/api/collections", {
+            method: "GET",
+        })
+        const data = await res.json()
+        setCollections(data)
+        setLoading(false)
+
+    } catch (err){
+        console.log("[collections_GET_error]", err);
+    }
+  }
+
+  useEffect(() => {
+    getCollections()
+  }, [])
+
   return (
-    <div>Collection</div>
+    <PageContainer>
+        <div className='space-y-2'>
+            <Breadcrumbs items={breadcrumbItems} />
+        </div>
+    </PageContainer>
   )
 }
 

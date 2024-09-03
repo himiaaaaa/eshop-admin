23205d12a1a1f35b97b85b2f32f970e70eaccfcb
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2024-09-03 12:12:34 +0800

    'get collections page' - delete button handledelete

--- components/delete-button.tsx
+++ components/delete-button.tsx
@@ -1,15 +1,81 @@
-import React from 'react'
-import { Button } from './ui/button'
-import { Trash } from 'lucide-react'
+"use client"
 
-const deleteButton = () => {
-  return (
-    <div>
-        <Button>
-          <Trash className="h-4 w-4" />
-        </Button>
-    </div>
-  )
+import { useState } from "react";
+
+import { Trash } from "lucide-react";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "./ui/button";
+import toast from "react-hot-toast";
+
+interface DeleteProps {
+  id: string
 }
 
-export default deleteButton
+const deleteButton: React.FC<DeleteProps> = ({ id }) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleDelete = async () => {
+    try {
+      setLoading(true)
+
+      const res = await fetch(`/api/collections/${id}`, {
+        method: "DELETE",
+      })
+
+      if (res.ok) {
+        setLoading(false)
+        window.location.href = (`/collections`)
+        toast.success(`collection successfully deleted`)
+      }
+    } catch (err) {
+      console.log(err)
+      toast.error("Something went wrong! Please try again.")
+    }
+  }
+
+
+  return (
+    <div>
+      
+      <AlertDialog>
+        <AlertDialogTrigger>
+          <Button>
+            <Trash className="h-4 w-4" />
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle className="text-red-700">Are you sure to delete collection?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              collection.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction 
+              className="bg-red-700 text-white"
+              onClick={handleDelete}
+            >
+              Delete
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
+    </div>
+  );
+};
+
+export default deleteButton;
